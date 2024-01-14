130df1716ecbb4a4d0efc5c59549154ee3d995ce
Voligoma <yacussijuancruz@gmail.com>
2024-01-13 23:31:27 -0300

    Update Main.py
    funciona pero no cambia el prefijo :anguished:

--- BOT/Main.py
+++ BOT/Main.py
@@ -6,14 +6,22 @@ class MyClient(discord.Client):
 		print(f'Logged on as {self.user}!')
 
 	async def on_message(self, message):
+		prefix = "-"
 		print(f'Message from {message.author}: {message.content}')
-		if message.content == "hello":
+		if message.content == prefix + "help":
 			#start = timeit.timeit()
-			await message.channel.send("World")
+			await message.channel.send("ayuda: para cambiar el prefijo el comando es: " + prefix + "prefix")
 			#end = timeit.timeit()
 			#totTime = end - start
 			#print("el timepo de ejecucion fue de  " + str(totTime) + "S")
-
+		else:
+			command, *args = message.content.split(' ', 1)
+			if command == prefix + "prefix":
+				if args:
+					prefix = args[0]
+					await message.channel.send(f"Prefijo cambiado a: {prefix}")
+				else:
+					await message.channel.send("Faltan argumentos")
 
 intents = discord.Intents.default()
 intents.message_content = True
