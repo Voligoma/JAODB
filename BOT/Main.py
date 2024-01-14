94d2e5155f782ce6b916d431e42d281eb32826d2
Voligoma <yacussijuancruz@gmail.com>
2024-01-13 23:51:54 -0300

    Eliminacion de token
    Eliminar el TOKEN del archivo principal y no comparitirlo XD
    skill issue

--- BOT/Main.py
+++ BOT/Main.py
@@ -1,25 +1,28 @@
 import discord
 import timeit
+from discord import app_commands
+from discord.ext import commands
+from config import TOKEN
 
 class MyClient(discord.Client):
+	prefix = "-"
 	async def on_ready(self):
 		print(f'Logged on as {self.user}!')
 
 	async def on_message(self, message):
-		prefix = "-"
 		print(f'Message from {message.author}: {message.content}')
-		if message.content == prefix + "help":
+		if message.content == self.prefix + "help":
 			#start = timeit.timeit()
-			await message.channel.send("ayuda: para cambiar el prefijo el comando es: " + prefix + "prefix")
+			await message.channel.send("ayuda: para cambiar el prefijo el comando es: " + self.prefix + "prefix")
 			#end = timeit.timeit()
 			#totTime = end - start
 			#print("el timepo de ejecucion fue de  " + str(totTime) + "S")
 		else:
 			command, *args = message.content.split(' ', 1)
-			if command == prefix + "prefix":
+			if command == self.prefix + "prefix":
 				if args:
-					prefix = args[0]
-					await message.channel.send(f"Prefijo cambiado a: {prefix}")
+					MyClient.prefix = args[0]
+					await message.channel.send(f"Prefijo cambiado a: {MyClient.prefix}")
 				else:
 					await message.channel.send("Faltan argumentos")
 
@@ -27,4 +30,4 @@ intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('MTA1Mjc1ODA4NTUzNTUzOTI1Mg.GKzulT.qldetCkzqz66V_iFDEcJf7uckdvAi0AGW4VIRY')
+client.run('TOKEN')
